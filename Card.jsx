cd157834ef3d878f50f149ce2d47c66bbe2fdeb8
Rimshadarya <rimshadarya@gmail.com>
2024-12-28 13:57:51 +0500

    Update Card.jsx

--- Card.jsx
+++ Card.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styles from "./Card.module.css";
 
-const Card = ({ id, title,price,description,category,image,rate,count }) => {
+const Card = ({ id, title, price, description, category, image, rating }) => {
   return (
-    <div className={styles.divContainer}>
-      <h4>{id}</h4>
-      <h2>{title}</h2>
-      <h1>{price}</h1>
-      <p>{description}</p>
-      <h6>{category}</h6>
-      <img className={styles.img}src={image}/>
-      {<p>{rate}</p>}
+    <div className={styles.card}>
+      <img className={styles.image} src={image} alt={title} />
+      <div className={styles.details}>
+        <h4 className={styles.id}>ID: {id}</h4>
+        <h2 className={styles.title}>{title}</h2>
+        <h3 className={styles.price}>Price: {price}</h3>
+        <p className={styles.description}>{description}</p>
+        <p className={styles.category}>Category: {category}</p>
+        <div className={styles.rating}>
+          <span>Rating: {rating.rate} </span>
+          <span>({rating.count} reviews)</span>
+        </div>
+      </div>
     </div>
   );
 };
